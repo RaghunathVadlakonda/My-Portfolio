a07efb2f2b6a44b1435e7cc9081035ecc08054c6
Raghunath Vadlakonda <raghu@Raghunaths-MacBook-Air.local>
2019-01-10 19:16:00 +0530

    menubutton and javascript logic for menubutton

---
+++ dist/js/main.js
@@ -0,0 +1,34 @@
+// select DOM 
+
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
+
+//Intial state of Menu
+
+let ShowMenu = false;
+ 
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if(!ShowMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+
+        ShowMenu = true;
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+
+        ShowMenu = false;
+    }
+}
+
